3523067ba0a64ffc4ea1c671826e8263de840707
fgdorais <fgdorais@users.noreply.github.com>
2025-06-09 03:25:39 +0000

    chore: update toolchain v4.21.0-rc3

--- Algebra/Basic.lean
+++ Algebra/Basic.lean
@@ -5,7 +5,3 @@ Released under Apache 2.0 license as described in the file LICENSE.
 import Batteries
 import Extra
 import Logic
-
-class Inv (α : Type _) where
-  inv : α → α
-postfix:max "⁻¹" => Inv.inv
